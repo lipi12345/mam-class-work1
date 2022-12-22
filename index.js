d78e229ca21a9027729ba0c8d582b9a98dca3953
lipi12345 <lipiaktermahi@gmail.com>
2022-12-22 10:50:13 +0600

    added

---
+++ index.js
@@ -0,0 +1,22 @@
+// const num1 = Number(prompt("enter your first number"));
+// const num2 = Number(prompt("enter your second number"));
+// const num3 = Number(prompt("enter your third number"));
+
+// function finalResult() {
+//   document.write("Addition is" + (num1 + num2 + num3) + "<br />");
+//   document.write("Subtraction is" + (num1 - num2 - num3) + "<br/>");
+//   document.write("Multiplication is" + num1 * num2 * num3 + "<br/>");
+//   document.write("Division is" + num1 / num2 / num3 + "<br />");
+// }
+// finalResult();
+
+let first = Number(prompt("Enter your first number"));
+let second = Number(prompt("Enter your secound number"));
+let third = Number(prompt("Enter your third number"));
+function resultFun(num1, num2, num3) {
+  document.write("<br/>" + "Enter Addition" + (num1 + num2 + num3));
+  document.write("<br/>" + "Enter subtraction" + (num1 - num2 - num3));
+  document.write("<br/>" + "Enter Multiplication" + num1 * num2 * num3);
+}
+
+resultFun(first, second, third);
